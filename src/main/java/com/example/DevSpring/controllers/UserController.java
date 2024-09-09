763e26f535e026229510134433fef1924c6ed602
Dao An Phuoc <anphuocdao2410@gmail.com>
2024-09-09 21:29:18 +0700

    CRUD

--- src/main/java/com/example/DevSpring/controllers/UserController.java
+++ src/main/java/com/example/DevSpring/controllers/UserController.java
@@ -1,8 +1,15 @@
 package com.example.DevSpring.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.DevSpring.dto.UserDTO;
@@ -10,12 +17,42 @@ import com.example.DevSpring.entity.User;
 import com.example.DevSpring.service.UserService;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@PostMapping("/users")
-	User creatUser(@RequestBody UserDTO request) {
+	@PostMapping
+	User createUser(@RequestBody UserDTO request) {
 		return userService.createUser(request);
 	}
+
+	@GetMapping
+	List<User> getUsers() {
+		return userService.getListUser();
+	}
+
+	@GetMapping("/{id}")
+	String findUser(@PathVariable Long id) {
+		User user = userService.findUserByID(id);
+		if(user != null) {
+			return "Found";
+		}else {
+			return "Not Found";
+		}
+	
+	}
+	
+	@PutMapping("/{id}")
+	User updateUser(@PathVariable Long id,@RequestBody UserDTO request) {
+		return userService.updateUserByID(id,request);
+	}
+	
+	@DeleteMapping("/{id}")
+	String deleteUser(@PathVariable Long id) {
+		userService.deleteUserByID(id);
+		return "Delete User Success";
+	}
+	
+
 }
--- src/main/java/com/example/DevSpring/service/UserService.java
+++ src/main/java/com/example/DevSpring/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.DevSpring.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +18,31 @@ public class UserService {
 		User user = new User();
 
 		user.setUsername(request.getUsername());
+		user.setEmail(request.getEmail());
+		user.setPassword(request.getPassword());
 
 		return userRepo.save(user);
 
+	}
+
+	public List<User> getListUser() {
+
+		return userRepo.findAll();
+	}
+
+	public User findUserByID(Long userID) {
+		return userRepo.findById(userID).orElse(null);
+	}
+
+	public User updateUserByID(Long id, UserDTO request) {
+		User user = findUserByID(id);
+		user.setUsername(request.getUsername());
+		user.setEmail(request.getEmail());
+		user.setPassword(request.getPassword());
+		return userRepo.save(user);
+	}
+	
+	public void deleteUserByID(Long id) {
+		 userRepo.deleteById(id);
 	}
 }
